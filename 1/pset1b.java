d958eac492a1a6c8fd47e4b1bbcb08aa9db7db94
excelmm <excel.1999@yahoo.com>
2020-12-06 23:52:17 +0700

    refactor

--- 1/pset1b.java
+++ 1/pset1b.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class pset1b {
 
     public static void main(String[] args) throws FileNotFoundException {
-        File file = new File("pset1.txt");
+        File file = new File("input.txt");
         Scanner sc = new Scanner(file);
         ArrayList<Integer> inputs = new ArrayList<Integer>();
 
