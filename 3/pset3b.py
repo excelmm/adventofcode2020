bb31cd890d263d8cafe61945f6dd59354aa21d23
excelmm <excel.1999@yahoo.com>
2020-12-06 23:23:38 +0700

    updated py solution

--- 3/pset3b.py
+++ 3/pset3b.py
@@ -1,22 +1,26 @@
+def compute(map, row, col):
+
+    rowlen = len(map)
+    collen = len(map[0])
+
+    count = 0
+    i = 0
+    j = 0
+
+    while True:
+        if map[i][j] == '#':
+            count += 1
+        i += row
+        j += col
+        if j >= collen:
+            j -= collen
+        if i >= rowlen:
+            break
+
+
+    return count
+
 with open("pset3.txt", "r") as f:
     map = f.read().splitlines()
 
-rowlen = len(map)
-collen = len(map[0])
-
-count = 0
-i = 0
-j = 0
-
-while True:
-    if map[i][j] == '#':
-        count += 1
-    i += 2
-    j += 1
-    if j >= collen:
-        j -= collen
-    if i >= rowlen:
-        break
-
-
-print(count)
+print(compute(map, 1, 1) * compute(map, 1, 3) * compute(map, 1, 5) * compute(map, 1, 7) * compute(map, 2, 1))
