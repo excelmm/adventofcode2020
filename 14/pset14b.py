59039a98afe0dc9f427ec0ffcad0bd1d8b991b78
excelmm <excel.1999@yahoo.com>
2020-12-14 14:23:57 +0700

    Day 14 Python solutions

--- 14/pset14b.py
+++ 14/pset14b.py
@@ -9,6 +9,8 @@ def main():
     sumofall = {}
     mask = ''
     for i in input:
+
+        # Parse inputs
         if 'mask' in i:
             mask = i[7:]
             continue
@@ -18,15 +20,22 @@ def main():
         
         initialAddressBinary = bin(int(initialAddress))[2:]
 
+        # Convert initial address to binary
         for i in range(len(mask) - len(initialAddressBinary)):
             initialAddressBinary = "0" + initialAddressBinary
+
+        # get the memmory address
         for i, char in enumerate(mask): 
             if char != '0':
                 initialAddressBinary = initialAddressBinary[:i] + char + initialAddressBinary[i + 1:]
+
+        # Find out indexes of "X" in the address
         Xindexes = []
         for i, char in enumerate(initialAddressBinary):
             if char == 'X':
                 Xindexes.append(i)
+
+        # Write for every possible combination of floating bits
         combs = list(powerset(Xindexes))
         for i in combs:
             newAddressBinary = copy.deepcopy(initialAddressBinary)
@@ -35,11 +44,14 @@ def main():
             newAddressBinary = newAddressBinary.replace("X", "0")
             sumofall[int(newAddressBinary, 2)] = int(initialValue)
 
+    # Sum final
     sum = 0
     for i, j in sumofall.items():
         sum += j       
     print(sum)
 
+
+# Find all subsets of a set
 def powerset(iterable):
     s = list(iterable)
     return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
